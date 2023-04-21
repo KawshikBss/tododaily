0ef44fb8e7a560a9a5fe076e8ed81c1e321cf84a
KawshikBss <kawshikbss@gmail.com>
2023-04-21 20:57:34 +0600

    modified get method to render tasks view

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", (req, res, next) => {
-    res.send({ tasks: "all" });
+    res.render("tasks");
 });
 
 module.exports = router;
