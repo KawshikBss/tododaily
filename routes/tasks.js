ed2fd9dcaa542d7d9e1e752e174cbec28e11a899
KawshikBss <kawshikbss@gmail.com>
2023-04-23 23:38:21 +0600

    modified get method and added post method

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,30 @@
 var express = require("express");
 var router = express.Router();
+var Task = require("../models/tasks");
 
-router.get("/", (req, res, next) => {
-    res.render("tasks");
+router.get("/", async (req, res, next) => {
+    try {
+        const tasks = await Task.find();
+        console.log(tasks);
+        res.render("tasks", { tasks: tasks });
+    } catch (err) {
+        console.error(err);
+    }
 });
+
+router.post("/", async (req, res, next) => {
+    const task = new Task({
+        title: req.body.title,
+        description: req.body.description,
+    });
+    try {
+        const newTask = await task.save();
+    } catch (err) {
+        console.error(err);
+    }
+    res.redirect("/tasks");
+});
+
+const getTaskByID = async (req, res, next) => {};
 
 module.exports = router;
