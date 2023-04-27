d5fcc20d388b1a25833f62f2d9c7ebee71580e57
KawshikBss <kawshikbss@gmail.com>
2023-04-27 23:24:15 +0600

    modified strategy callback

--- strategy.js
+++ strategy.js
@@ -1,5 +1,25 @@
 var LocalStrategy = require("passport-local");
+var User = require("./models/users");
+var bcrypt = require("bcrypt");
 
-var strategy = new LocalStrategy();
+var strategy = new LocalStrategy(async (username, password, cb) => {
+    try {
+        var user = await User.findOne({ username: username });
+        if (!user) {
+            return cb(null, false, {
+                message: "Incorrect username or password.",
+            });
+        }
+        var validUser = await bcrypt.compare(password, user.password);
+        if (!validUser) {
+            return cb(null, false, {
+                message: "Incorrect username or password.",
+            });
+        }
+        return cb(null, user);
+    } catch (err) {
+        return cb(err);
+    }
+});
 
 module.exports = strategy;
