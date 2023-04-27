8e3d0b8ed6035208840590d87e7c5cc4bf0c8299
KawshikBss <kawshikbss@gmail.com>
2023-04-27 23:16:57 +0600

    modified post route for user creation

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var router = express.Router();
+var User = require("../models/users");
+var bcrypt = require("bcrypt");
 
 router.get("/signin", (req, res, next) => {
     res.render("signin");
@@ -7,6 +9,31 @@ router.get("/signin", (req, res, next) => {
 
 router.get("/signup", (req, res, next) => {
     res.render("signup");
+});
+
+router.post("/signup", async (req, res, next) => {
+    var userExists = await User.findOne({ email: req.body.email });
+    if (userExists) {
+        return res.redirect("/auth/singup");
+    }
+    try {
+        var salt = await bcrypt.genSalt(10);
+        var hashedPassword = await bcrypt.hash(req.body.password, salt);
+        var user = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+        });
+        user = await user.save();
+    } catch (err) {
+        console.error(err);
+    }
+    req.login(user, (err) => {
+        if (err) {
+            return res.redirect("/auth/singup");
+        }
+        return res.redirect("/");
+    });
 });
 
 module.exports = router;
