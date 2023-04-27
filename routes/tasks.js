701cd6cc62065cf31f2460d17630f7e1e68319dd
KawshikBss <kawshikbss@gmail.com>
2023-04-27 23:43:22 +0600

    filtered tasks for session user and added user id to task

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,7 +5,7 @@ var Task = require("../models/tasks");
 router.get("/", async (req, res, next) => {
     try {
         var user = req.user;
-        const tasks = await Task.find();
+        const tasks = await Task.find({ user_id: user.id });
         res.render("tasks", { user: user, tasks: tasks });
     } catch (err) {
         console.error(err);
@@ -13,11 +13,13 @@ router.get("/", async (req, res, next) => {
 });
 
 router.post("/", async (req, res, next) => {
-    const task = new Task({
-        title: req.body.title,
-        description: req.body.description,
-    });
     try {
+        var user = req.user;
+        const task = new Task({
+            user_id: user.id,
+            title: req.body.title,
+            description: req.body.description,
+        });
         const newTask = await task.save();
     } catch (err) {
         console.error(err);
